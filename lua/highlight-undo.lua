a87ccea4dcee0139c1146931a23fb36e5459fff7
aileot <aileot.foobar@gmail.com>
2024-06-03 22:23:23 +0900

    refactor(big-change): extend highlight occasions other than undo/redo

--- lua/highlight-undo.lua
+++ lua/highlight-undo.lua
@@ -1,48 +1,29 @@
 -- This module highlights reference usages and the corresponding
 -- definition on cursor hold.
 
-local api = vim.api
-
 local M = {
   config = {
-    duration = 300,
-    undo = {
-      hlgroup = 'HighlightUndo',
-      mode = 'n',
-      lhs = 'u',
-      map = 'undo',
-      opts = {},
-    },
-    redo = {
-      hlgroup = 'HighlightRedo',
-      mode = 'n',
-      lhs = '<C-r>',
-      map = 'redo',
-      opts = {},
-    },
-    highlight_for_count = true,
+    duration = 400,
+    hlgroup = { added = "HlBigChangeAdded", removed = "HlBigChangeRemoved" },
   },
   timer = (vim.uv or vim.loop).new_timer(),
-  should_detach = true,
-  current_hlgroup = nil,
 }
 
-local usage_namespace = api.nvim_create_namespace('highlight_undo')
+local namespace = vim.api.nvim_create_namespace("HlBigChange")
 
-function M.call_original_kemap(map)
-  if type(map) == 'string' then
-    vim.cmd(map)
-  elseif type(map) == 'function' then
-    map()
+---makes highlight-undo respect `foldopen=undo` (#18)
+local function open_folds_on_undo()
+  if vim.tbl_contains(vim.opt.foldopen:get(), "undo") then
+    vim.cmd.normal({ "zv", bang = true })
   end
 end
 
-function M.on_bytes(
+local function on_bytes(
   ignored, ---@diagnostic disable-line
   bufnr, ---@diagnostic disable-line
   changedtick, ---@diagnostic disable-line
   start_row, ---@diagnostic disable-line
-  start_column, ---@diagnostic disable-line
+  start_col, ---@diagnostic disable-line
   byte_offset, ---@diagnostic disable-line
   old_end_row, ---@diagnostic disable-line
   old_end_col, ---@diagnostic disable-line
@@ -51,16 +32,13 @@ function M.on_bytes(
   new_end_col, ---@diagnostic disable-line
   new_end_byte ---@diagnostic disable-line
 )
-  if M.should_detach then
-    return true
-  end
-  -- dump(
+  -- vim.print(
   --   {
   --     ignored = ignored,
   --     bufnr = bufnr,
   --     changedtick = changedtick,
   --     start_row = start_row,
-  --     start_column = start_column,
+  --     start_col = start_col,
   --     byte_off = byte_offset,
   --     old_end_row = old_end_row,
   --     old_end_col = old_end_col,
@@ -70,42 +48,39 @@ function M.on_bytes(
   --     new_end_byte = new_end_byte,
   --   }
   -- )
-  -- defer highligh till after changes take place..
-  local num_lines = api.nvim_buf_line_count(0)
-  local end_row = start_row + new_end_row
-  local end_col = start_column + new_end_col
-  if end_row >= num_lines then
-    -- we are past the last line. highligh till the last column
-    end_col = #api.nvim_buf_get_lines(0, -2, -1, false)[1]
+  if
+    not vim.api.nvim_get_mode().mode:find("n")
+    -- (old_end_row < start_row and new_end_row < start_row)
+    or (
+      old_end_row == start_row
+      and new_end_row == start_row
+      and old_end_col <= new_end_col + 1
+    )
+  then
+    -- Skip if single char change
+    return
   end
+  -- defer highlight till after changes take place..
+  local end_row = start_row + new_end_row
+  local end_col = start_col + new_end_col
+  local num_lines = vim.api.nvim_buf_line_count(0)
+  if end_row >= num_lines then
+    -- we are past the last line. highlight till the last column
+    end_col = #vim.api.nvim_buf_get_lines(0, -2, -1, false)[1]
+  end
+  open_folds_on_undo()
+  local hlgroup = M.config.hlgroup.added
+  -- TODO: Show extmark for Removed texts.
   vim.schedule(function()
     vim.highlight.range(
       bufnr,
-      usage_namespace,
-      M.current_hlgroup,
-      { start_row, start_column },
-      { end_row, end_col}
+      namespace,
+      hlgroup,
+      { start_row, start_col },
+      { end_row, end_col }
     )
     M.clear_highlights(bufnr)
   end)
-  --detach
-  -- return true
-end
-
-function M.highlight_undo(bufnr, hlgroup, command)
-  M.current_hlgroup = hlgroup
-  api.nvim_buf_attach(bufnr, false, {
-    on_bytes = M.on_bytes,
-  })
-  M.should_detach = false
-  if M.config.highlight_for_count then
-    for _ = 1, vim.v.count1 do
-      command()
-    end
-  else
-    command()
-  end
-  M.should_detach = true
 end
 
 function M.clear_highlights(bufnr)
@@ -114,57 +89,32 @@ function M.clear_highlights(bufnr)
     M.config.duration,
     0,
     vim.schedule_wrap(function()
-      api.nvim_buf_clear_namespace(bufnr, usage_namespace, 0, -1)
+      vim.api.nvim_buf_clear_namespace(bufnr, namespace, 0, -1)
     end)
   )
 end
 
----makes highlight-undo respect `foldopen=undo` (#18)
-local function openFoldsOnUndo()
-  if vim.tbl_contains(vim.opt.foldopen:get(), "undo") then
-    vim.cmd.normal({"zv",bang = true})
-  end
-end
-
 function M.setup(config)
-  api.nvim_set_hl(0, 'HighlightUndo', {
-    fg = '#dcd7ba',
-    bg = '#2d4f67',
+  vim.api.nvim_set_hl(0, "HlBigChangeAdded", {
+    fg = "#dcd7ba",
+    bg = "#2d4f67",
     default = true,
   })
-  api.nvim_set_hl(0, 'HighlightRedo', {
-    fg = '#dcd7ba',
-    bg = '#2d4f67',
+  vim.api.nvim_set_hl(0, "HlBigChangeRemoved", {
+    fg = "#2d4f67",
+    bg = "#dcd7ba",
     default = true,
   })
-
-  M.config = vim.tbl_deep_extend('keep', config or {}, M.config)
-
-  local undo = M.config.undo
-  vim.keymap.set(undo.mode, undo.lhs, function()
-    if M.config.highlight_for_count or vim.v.count == 0 then
-      M.highlight_undo(0, undo.hlgroup, function()
-        M.call_original_kemap(undo.map)
-      end)
-    else
-      local keys = vim.api.nvim_replace_termcodes(vim.v.count .. 'u', true, false, true)
-      vim.api.nvim_feedkeys(keys, 'n', false)
-    end
-    openFoldsOnUndo()
-  end, undo.opts)
-
-  local redo = M.config.redo
-  vim.keymap.set(redo.mode, redo.lhs, function()
-    if M.config.highlight_for_count or vim.v.count == 0 then
-      M.highlight_undo(0, redo.hlgroup, function()
-        M.call_original_kemap(redo.map)
-      end)
-    else
-      local keys = vim.api.nvim_replace_termcodes(vim.v.count .. '<c-r>', true, false, true)
-      vim.api.nvim_feedkeys(keys, 'n', false)
-    end
-    openFoldsOnUndo()
-  end, redo.opts)
+  M.config = vim.tbl_deep_extend("keep", config or {}, M.config)
+  local id = vim.api.nvim_create_augroup("HlBigChangeAdded", {})
+  vim.api.nvim_create_autocmd("BufWinEnter", {
+    group = id,
+    callback = function(a)
+      vim.api.nvim_buf_attach(a.buf, false, {
+        on_bytes = on_bytes,
+      })
+    end,
+  })
 end
 
 return M
