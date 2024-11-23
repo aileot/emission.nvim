a59d19ca120d8d292aa2a7bc13088c96d69d537f
aileot <46470475+aileot@users.noreply.github.com>
2024-11-23 19:06:42 +0900

    fix(option): set different highlight priorities of "added" and "removed"

--- lua/emission.lua
+++ lua/emission.lua
@@ -5,7 +5,7 @@ local function _2_()
 end
 local function _3_()
 end
-default_config = {attach = {delay = 100, excluded_filetypes = {}, excluded_buftypes = {"help", "nofile", "terminal", "prompt"}}, highlight_delay = 10, added = {hl_map = {default = true, bold = true, fg = "#dcd7ba", bg = "#2d4f67"}, priority = 101, duration = 300, filter = _2_}, removed = {hl_map = {default = true, bold = true, fg = "#dcd7ba", bg = "#672d2d"}, priority = 101, duration = 300, filter = _3_}}
+default_config = {attach = {delay = 100, excluded_filetypes = {}, excluded_buftypes = {"help", "nofile", "terminal", "prompt"}}, highlight_delay = 10, added = {hl_map = {default = true, bold = true, fg = "#dcd7ba", bg = "#2d4f67"}, priority = 102, duration = 300, filter = _2_}, removed = {hl_map = {default = true, bold = true, fg = "#dcd7ba", bg = "#672d2d"}, priority = 101, duration = 300, filter = _3_}}
 local cache = {config = vim.deepcopy(default_config), namespace = vim.api.nvim_create_namespace("emission"), timer = vim.uv.new_timer(), ["pending-highlights"] = Stack.new(), ["hl-group"] = {added = "EmissionAdded", removed = "EmissionRemoved"}, ["last-duration"] = 0, ["last-editing-position"] = {0, 0}, ["buf->detach?"] = {}, ["last-recache-time"] = 0, ["buf->old-texts"] = {}}
 local vim_2fhl = (vim.hl or vim.highlight)
 local function inc(x)
