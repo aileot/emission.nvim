f8ef16e58c763bf642d0c1ad485314df6d9a9091
aileot <46470475+aileot@users.noreply.github.com>
2024-12-07 16:42:00 +0900

    refactor(config): prepare default `on_events`

--- lua/emission/config.lua
+++ lua/emission/config.lua
@@ -66,6 +66,12 @@ local default_config = {
       return true
     end,
   },
+  ---@alias AutocmdEvent string|string[] the 1st arg of `nvim_create_autocmd()
+  ---@alias AutocmdOpts table the 2nd arg of `nvim_create_autocmd()`
+  ---@alias emission.Config.OnEvents table<AutocmdEvent,AutocmdOpts[]>
+  --- A option to help create autocmds dedicated to emission.
+  ---@type emission.Config.OnEvents
+  on_events = {},
 }
 
 M._config = default_config
