8bcff358d5f1fb010549a0d80d5c60eae64d1eca
aileot <46470475+aileot@users.noreply.github.com>
2024-12-07 10:09:02 +0900

    refactor(config): remove NYI options
    They only cheats LS.

--- lua/emission/config.lua
+++ lua/emission/config.lua
@@ -3,9 +3,6 @@ local logger = require("emission.utils.logger")
 local M = {}
 local user_config
 
----@class emission.Filter.Context
----@field buf integer
-
 ---@alias HlMap table<string,any> options for the 3rd arg of `nvim_set_hl()`
 
 ---@class emission.Config
@@ -23,7 +20,6 @@ local default_config = {
   highlight = {
     duration = 300,
     min_byte = 2,
-    min_row_offset = 0,
     filter = function(_buf)
       return true
     end,
@@ -36,24 +32,12 @@ local default_config = {
     priority = 102,
     ---@type HlMap options for the 3rd arg of `nvim_set_hl()`
     hl_map = { default = true, bold = true, fg = "#dcd7ba", bg = "#2d4f67" },
-    ---@param ctx emission.Filter.Context
-    ---@return boolean Return false or nil to ignore; otherwise, highlight
-    --- texts
-    filter = function(ctx)
-      return true
-    end,
   },
   ---@class emission.Config.Removed
   removed = {
     priority = 101,
     ---@type HlMap options for the 3rd arg of `nvim_set_hl()`
     hl_map = { default = true, bold = true, fg = "#dcd7ba", bg = "#672d2d" },
-    ---@param ctx emission.Filter.Context
-    ---@return boolean Return false or nil to ignore; otherwise, highlight
-    --- texts
-    filter = function(ctx)
-      return true
-    end,
   },
 }
 
