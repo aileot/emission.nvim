01f1fb9c0f70bdec218bbbd6dcd4641e85ad346c
aileot <46470475+aileot@users.noreply.github.com>
2025-01-05 14:03:12 +0900

    docs(setup): annotate args at the end (#40)

--- lua/emission/init.lua
+++ lua/emission/init.lua
@@ -353,10 +353,10 @@ local function create_autocmds_21()
   return nil
 end
 
----@param opts? emission.Config
 --- Initialize emission.
---- Your options are always merged into the default config,
---- not the current config.
+--- Your options are always merged into the default config, not the
+--- current config.
+---@param opts? emission.Config
 local function setup(opts)
   local opts0 = (opts or {})
   config.merge(opts0)
